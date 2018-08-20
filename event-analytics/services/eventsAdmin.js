a0e0abf73b73ca267da40476c7dc1c4ee88d726f
prasannacs <prasannacs.dev@gmail.com>
2018-08-20 18:17:49 -0400

    Create eventsAdmin.js

---
+++ event-analytics/services/eventsAdmin.js
@@ -0,0 +1,38 @@
+// Imports the Google Cloud client library
+const BigQuery = require('@google-cloud/bigquery');
+const Request = require("request");
+
+// Your Google Cloud Platform project ID
+const projectId = 'sixth-hawk-194719';
+
+// Creates a client
+const bigquery = new BigQuery({
+    projectId: projectId,
+});
+
+module.exports = {
+
+
+function callEventsAPI(accessToken) {
+
+
+    var eventURL = 'https://api.box.com/2.0/events?stream_type=admin_logs';
+
+    var options = {
+        method: 'GET',
+        url: eventURL,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization' : 'Bearer '+accessToken
+        }
+    }
+    
+    Request(options, callback);
+
+}
+}
+
+function callback(error, response, body) {
+    console.log('response ',response);
+
+}
