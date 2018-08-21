8b28e7da57b36fe3cbde68e5cfbd3c9a9cc9c318
prasannacs <prasannacs.dev@gmail.com>
2018-08-21 10:11:09 -0400

    Update eventsAdmin.js

--- event-analytics/services/eventsAdmin.js
+++ event-analytics/services/eventsAdmin.js
@@ -11,28 +11,25 @@ const bigquery = new BigQuery({
 });
 
 module.exports = {
+    callEventsAPI: function(accessToken) {
+        var eventURL = 'https://api.box.com/2.0/events?stream_type=admin_logs';
 
-
-function callEventsAPI(accessToken) {
-
-
-    var eventURL = 'https://api.box.com/2.0/events?stream_type=admin_logs';
-
-    var options = {
-        method: 'GET',
-        url: eventURL,
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization' : 'Bearer '+accessToken
+        var options = {
+            method: 'GET',
+            url: eventURL,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + accessToken
+            }
         }
-    }
-    
-    Request(options, callback);
 
-}
+        Request(options, callback);
+
+    }
+
 }
 
 function callback(error, response, body) {
-    console.log('response ',response);
+    console.log('response ', response);
 
 }
