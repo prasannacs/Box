fd5a3a0184fa768ac7635f077380281be59e1fd8
prasannacs <prasannacs.dev@gmail.com>
2019-01-14 10:44:51 -0500

    Update eventsAdmin.js

--- event-analytics/services/eventsAdmin.js
+++ event-analytics/services/eventsAdmin.js
@@ -49,7 +49,7 @@ function getBQDate(date)    {
          var bq_date = BigQuery.datetime({
          year: date.getFullYear(),
           month: date.getMonth(),
-          day: date.getDay(),
+          day: date.getDate(),
           hours: date.getHours(),
           minutes: date.getMinutes(),
           seconds: date.getSeconds()
@@ -76,7 +76,7 @@ function callback(error, response, body) {
         var created_date = new Date(entries[counter].created_at);
          var bq_created_date = getBQDate(created_date);
         var insert_date = new Date();
-        insert_date.setDate(insert_date.getDate()-1)
+  //      insert_date.setDate(insert_date.getDate()-1)
         var event_admin_row = {event_id: entries[counter].event_id, created_at: bq_created_date, event_type: entries[counter].event_type, ip_address: entries[counter].ip_address, session_id: entries[counter].session_id, inserted_at: getBQDate(insert_date), eid: eid};
         event_admin_rows.push(event_admin_row);
         console.log('event_admin_row -',event_admin_row);
