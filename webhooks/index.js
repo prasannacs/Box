efc619dc01e410108414a677dc8a1bfe7b7eb369
prasannacs <prasannacs.dev@gmail.com>
2019-03-18 12:23:00 -0400

    Update index.js

--- webhooks/index.js
+++ webhooks/index.js
@@ -5,6 +5,13 @@
  * @param {!express:Response} res HTTP response context.
  */
 const BoxSDK = require('box-node-sdk');
+const jsonConfig = require('https://github.com/prasannacs/Box/blob/master/webhooks/CLI-private.json');
+var sdk = BoxSDK.getPreconfiguredInstance(jsonConfig);
+var client = sdk.getAppAuthClient('enterprise');
+
+sdk.getEnterpriseAppAuthTokens('59194496', null, function (error, token) {
+    console.log('Service account token ', token.accessToken);
+})
 
 exports.webhookTrigger = (req, res) => {
   let message = req.query.message || req.body.message || 'Hello World!';
